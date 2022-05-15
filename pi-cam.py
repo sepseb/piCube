c27d70c5eb6983fd78f795fc2f421a3f68ff1cb3
Sep <sepzed@hotmail.com>
2022-05-14 23:04:13 -0700

    Update pi-cam.py

--- pi-cam.py
+++ pi-cam.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from importlib.resources import path
 from picamera import PiCamera
 from datetime import datetime
 from time import sleep
@@ -6,14 +8,15 @@ import subprocess
 import os
 
 default_path = '/home/pi/Documents/'
+session_path = '/home/pi/Documents/'
 camera = PiCamera()
 
 class PiShell(Cmd):
     prompt = 'PiCube $ '
     intro = "\n--------------------------------------"\
-            "\n PiCube Camera Interface Software"\
-            "\n Enter help for a list of commands"\
-            "\n Enter exit to leave the program"
+            "\n PiCube Interface Program"\
+            "\n Type help for a list of commands"\
+            "\n Type exit to leave the program"
 
     def do_exit(self,inp):
         # Add code to stop any running tasks with camera
@@ -22,17 +25,18 @@ class PiShell(Cmd):
     def help_exit(self):
         print(f'Exit the program')
 
-    def do_create_dir(self,inp):
+    def do_new_session(self,inp):
         path =  default_path + inp
         if (os.path.isfile(path) == False):
             os.mkdir(path)
+            session_path = path
             print(f'Created directory : {path}')
         else:
             print(f'Directory Already Exists')
 
-    def help_create_dir(self):
-        print(f'Syntax : create_dir [folder name]')
-        print(f'Create directory for pictures')
+    def help_new_session(self):
+        print(f'Syntax : new_session [folder name]')
+        print(f'Creates a new folder in Documents')
 
     def do_capture(self,inp):
         if (inp == 'ts'):
@@ -46,7 +50,7 @@ class PiShell(Cmd):
         camera.capture(image_path)
 
     def help_capture(self):
-        print(f'Syntax : capture [x]')
+        print(f'Syntax : capture x')
         print(f'Captures one picture ')
         print(f'If x = ts, filename is current data_time.jpg')
         print(f'If x = gps, filename is current lat_lon.jpg')
@@ -59,7 +63,7 @@ class PiShell(Cmd):
             camera.capture(image_path % i)
 
     def help_burst_mode(self):
-        print(f'Syntax : burst_mode [x] [y]')
+        print(f'Syntax : burst_mode x y')
         print(f'Captures total of x pictures; one picture every y seconds')
 
     def do_transfer(self, inp):
@@ -68,9 +72,6 @@ class PiShell(Cmd):
 
     def help_transfer(self):
         print(f'Transfers images to the computer')    
-
-    do_EOF = do_exit
-    help_EOF = help_exit
 
 def parse(arg):
     'Convert a series of zero or more numbers to an argument tuple'
