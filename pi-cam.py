b4d3d2a39c6ab5a76da41cd57eaeb63ca643464b
Sep <sepzed@hotmail.com>
2022-07-09 09:34:40 -0700

    Added Raw Capture

--- pi-cam.py
+++ pi-cam.py
@@ -4,13 +4,15 @@ from importlib.resources import path
 from ssl import SSLSession
 from matplotlib import use
 from picamera import PiCamera
-from datetime import datetime
+from picamera.array import PiBayerArray
+from datetime import datetie
 from adafruit_servokit import ServoKit
 from time import sleep
 from cmd import Cmd
 import subprocess
 import os
 import configparser
+import numpy as np
 
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
@@ -43,16 +45,15 @@ elif (picam_version == 'imx477'):
 else:
     print(f'Camera Model Unknown or no camera detected')
 
-print(f'Camera Resolution set to maximum {camera.resolution}')
+print(f'Camera resolution set to maximum {camera.resolution}')
 
 class PiShell(Cmd):
 
     prompt = 'PiCube $ '
     intro = "\n--------------------------------------"\
-            "\n PiCube Interface Program V 0.1.0"\
+            "\n PiCube Interface Program v0.1.0"\
             "\n Type help for a list of commands"\
             "\n Type exit to leave the program"\
-
 
     def do_exit(self,inp):
         # Add code to stop any running tasks with camera
@@ -60,6 +61,23 @@ class PiShell(Cmd):
 
     def help_exit(self):
         print(f'Exit the program')
+
+    def do_cap_raw(self, inp):
+        global session_path
+        arg = parse(inp)
+        now = datetime.now()
+        dt_string = now.strftime("%m-%d-%Y_%H-%M-%S")
+        # Use Timestamp as image name
+        image_path = session_path + dt_string + '.raw'
+        with PiBayerArray(camera) as stream:
+            camera.capture(stream, 'jpeg', bayer=True)
+            output = (stream.demosaic() >> 2).astype(np.uint8)
+            with open(image_path, 'wb') as f:
+                output.tofile(f)
+       
+    def help_cap_raw(self):
+        print(f'Syntax : cap_raw')
+        print(f'Takes a raw image')
 
     def do_new_session(self,inp):
         global session_path
@@ -119,7 +137,8 @@ class PiShell(Cmd):
         print(f'Image Captured : {image_path}')
 
     def help_cap(self):
-        print(f'Syntax : cap x y')
+        print(f'Syntax : cap x')
+        print(f'Where x is iso')
 
     def do_burst_mode(self, inp):
         global session_path
@@ -144,7 +163,7 @@ class PiShell(Cmd):
         print(f'Sets the camera resolution to x by y')
         print(f'Camera Module v1 Max Resolution is (2592x1944)')
         print(f'Camera Module v2 Max Resolution is (32802464)')
-        print(f'HQ Camera Max Resolution is (4056x3040)')  
+        print(f'HQ Camera Max Resolution is (4056x3040)') 
 
     def do_filter(self, inp):
         if (inp == filter_name_1):
